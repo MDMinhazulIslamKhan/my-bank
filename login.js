69c38846e0a471a1ab093bfa9a96833ea661736f
MDMinhazulIslamKhan <minhazulislamkhan@gmail.com>
2022-10-02 14:07:51 +0600

    password change

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 document.getElementById('login-submit').addEventListener('click', function () {
     const emailAddress = document.getElementById('login-email').value;
     const loginPassword = document.getElementById('login-password').value;
-    if (emailAddress == 'abc' && loginPassword == 'fy') {
+    if (emailAddress == 'abu@gmail.com' && loginPassword == 'abusayeed') {
         window.location.href = 'bank.html'
     }
     else {
